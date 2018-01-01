93d8785658546f0092bf2ab72d28b5b06fe5a927
hang-on <asje@me.com>
2018-01-01 15:03:29 +0100

    Refactor more

--- main.asm
+++ main.asm
@@ -22,7 +22,8 @@
 .section "main" free
 ; -----------------------------------------------------------------------------
   init:
-    ; Run this function once (on game load).
+    ; Run this function once (on game load). Assume we come here from bluelib
+    ; boot code with ram, vram, mappers and registers initialized.
     ; Load the pico-8 palette to colors 16-31.
     ld a,SPRITE_PALETTE_START
     ld b,PICO8_PALETTE_SIZE
@@ -37,6 +38,11 @@
     SELECT_BANK FONT_BANK
     ld hl,font_table
     call load_vram_from_table
+    ;
+    ; Blank left column.
+    ld a,SCROLL_0__LCB_1_LINE_0_SPRITES_0
+    ld b,REGISTER_0
+    call set_register
     ;
     ; Perform test: Is this the first time ever this game is run?
     SELECT_EXTRAM
